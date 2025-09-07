ae1dbdbf692e17310722963b9eb2e29e537e0d2a
StanislavDidus <stasdidus9@gmail.com>
2025-09-07 13:39:41 +0200

    Init

---
+++ game.cpp
@@ -0,0 +1,33 @@
+#include "game.h"
+#include "surface.h"
+#include <cstdio> //printf
+
+namespace Tmpl8
+{
+	// -----------------------------------------------------------
+	// Initialize the application
+	// -----------------------------------------------------------
+	void Game::Init()
+	{
+	}
+	
+	// -----------------------------------------------------------
+	// Close down application
+	// -----------------------------------------------------------
+	void Game::Shutdown()
+	{
+	}
+
+	static Sprite rotatingGun(new Surface("assets/aagun.tga"), 36);
+	static int frame = 0;
+
+	// -----------------------------------------------------------
+	// Main application tick function
+	// -----------------------------------------------------------
+	void Game::Tick(float deltaTime)
+	{
+		screen->Clear(0);
+
+
+	}
+};
